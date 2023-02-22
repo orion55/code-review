fb5ea18f6501a6542a2234519035e29d1e6fbb49
oleg.grebenev <oleg.grebenev@rokolabs.com>
2023-02-22 13:00:55 +0500

    Add comment

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+/*Реализовать функциональность с помощью которой можно будет получать данные пользователей в нужном формате.
+На вход будет передаваться userId в виде строки. Данные лежат в /api/users/{userId}.
+В зависимости от роли пользователя должны возвращаться разные данные.
+Таких форматов сейчас 2:
+Если роль admin, то возвращаются поля name, role, password(хеш-пароля) и permissions. Если роль regular, то name и role.
+Форматы в дальнейшем могут изменяться.
+
+Поле permissions должно возвращаться в kebab-case виде и разбираться только из валидных значений: reportStats, validate.
+Эти поля тоже могут в дальнейшем изменяться.*/
+
 export async function getUserData(userId) {
     const user = await fetch(`/api/users/${userId}`)
     const rights = { reportStats: true, validate: true, };
